03c64e0bcfd0b288e3ad0ddf1fed1e63c8ec432b
Barış <bariscanx2@gmail.com>
2020-06-19 14:23:17 +0300

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterSampleComponent } from './components/counter-sample/counter-sample.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterSampleComponent
   ],
   imports: [
     BrowserModule,
